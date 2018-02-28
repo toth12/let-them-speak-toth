44319148f6bc89b15d7f459101e6935cdf59c84f
Monica Ong Reed <monica.ong@yale.edu>
2018-02-28 17:18:33 -0500

    home style updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,13 @@ const Home = props => (
     <div className='top'>
       <div className='brand' />
       <div>
-        <div className='welcome'>A data-driven anthology of oral history <br/>interviews with Holocaust survivors</div>
+        <div className='welcome'>A data-driven anthology of oral history interviews with Holocaust survivors</div>
       </div>
       <Link className='button gold' to='/about'>Let's explore</Link>
     </div>
     <div className='bottom'>
       <hr/>
-      <p>A project by Gabor Mihaly Toth in collaboration <br/>with the Yale Digital Humanities Lab</p>
+      <p>A project by Gabor Mihaly Toth in collaboration with the Yale Digital Humanities Lab</p>
     </div>
   </div>
 )
