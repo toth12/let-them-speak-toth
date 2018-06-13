c2d2ef1ffa1459140e2fd9be56654ea98e2d1067
duhaime <douglas.duhaime@gmail.com>
2018-06-13 13:54:38 -0400

    remove whitespace for linux hosts

--- server/docker/run_with_seed_data.sh
+++ server/docker/run_with_seed_data.sh
@@ -8,19 +8,14 @@ docker run -d \
   -p 7082:7082 \
   -p 8080:8080 \
   -p 27017:27017 \
-
   letthemspeak /bin/sh -c \
   "rm -rf /etc/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_dsa_key && \
   ssh-keygen -A && \
   mkdir -p /var/run/sshd && \
   /usr/sbin/sshd -e $@ & \
-
   mongod & \
-
   /usr/local/tomcat/bin/catalina.sh start & \
-
   npm run seed & \
-
   gunicorn -b 0.0.0.0:7082 \
   --workers 1 \
   --threads 8 \
