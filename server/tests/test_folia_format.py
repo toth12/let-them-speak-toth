fc64897977d79b9864e705708546941f7bfd928c
duhaime <douglas.duhaime@gmail.com>
2018-06-28 14:06:48 -0400

    test all fields on a sample of folia records

--- server/tests/test_folia_format.py
+++ server/tests/test_folia_format.py
@@ -9,13 +9,17 @@ from bs4 import BeautifulSoup
 [sys.path.append(i) for i in ['.', '..', 'server']]
 from build_db import folia_dir
 
+# sample one out of every `sample_rate` - set to 1 to test all folia records
+sample_rate = 500
+
 def test_folia_format():
   '''
   Ensure all folia XML files have the requred structure
   '''
   pool = Pool()
-  folia_files = glob(os.path.join(folia_dir, '*'))
-  for idx, _ in enumerate(pool.imap(validate_file, folia_files)):
+  all_files = glob(os.path.join(folia_dir, '*'))
+  sample = [i for idx, i in enumerate(all_files) if idx % sample_rate == 0]
+  for idx, _ in enumerate(pool.imap(validate_file, sample)):
     print(' * validating folia file', idx)
 
 def validate_file(folia_path):
@@ -31,6 +35,9 @@ def validate_file(folia_path):
     metadata = soup.find('metadata')
     assert metadata.find({'meta': {'id': 'testimony_id'}}).get_text()
     assert metadata.find({'meta': {'id': 'shelfmark'}}).get_text()
+    assert metadata.find({'meta': {'id': 'ghetto'}})
+    assert metadata.find({'meta': {'id': 'camp'}})
+    assert metadata.find({'meta': {'id': 'gender'}})
 
     # validate text content present
     text = soup.find('text')
