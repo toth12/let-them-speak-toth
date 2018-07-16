fe18109604527a942632dbd7df0e8ead832825d4
mongmedia <monica.ong@yale.edu>
2018-07-16 16:28:20 -0400

    start of fragment styling

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -6,7 +6,7 @@ const Tree = props => (
     <div className='left background-image'>
       <div className='text'>
         <h2>Anthology</h2>
-        <div className='welcome'>Select a word to explore an experience domain</div>
+        <div className='welcome'>Select a word to explore<br/>an experience domain</div>
       </div>
     </div>
     <div className='right'>
