c7da8f6d941994e3c7d39ada9de248cb2f6d6c3a
Jake Kara <jake@jakekara.com>
2020-08-21 15:55:29 -0400

    Removed server/docker/2020

--- server/docker/2020/build-app-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "* Creating docker image: 'letthemspeak-app'";
-
-# build the container 
-docker build \
-    --tag letthemspeak-app:latest \
-    --file lts-1-app.Dockerfile \
-    .
