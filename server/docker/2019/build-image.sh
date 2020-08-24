90e277399686c8d972f6c7f278a8bd1257d0ab67
Jake Kara <jake@jakekara.com>
2020-08-24 10:46:36 -0400

    Removed 2019 docker folder

--- server/docker/2019/build-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo " * Creating a docker image named 'letthemspeak'";
-
-# build the container
-docker build --no-cache --tag letthemspeak:latest --file Dockerfile .
--- server/docker/2019/open-shell.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# open a shell in a running docker container
-
-docker exec -it letthemspeak /bin/sh
