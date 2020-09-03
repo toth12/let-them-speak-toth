552c5ace8e0987bfddf733eec5615c5674886349
Jake Kara <jake@jakekara.com>
2020-09-03 14:37:36 -0400

    load essays from markdown files

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const path = require('path');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const path = require("path");
 
 const paths = {
-  src: path.resolve(__dirname, 'src'),
-  build: path.resolve(__dirname, 'build')
-}
+  src: path.resolve(__dirname, "src"),
+  build: path.resolve(__dirname, "build"),
+};
 
 const pathsToCopy = [
   {
     context: path.join(paths.src),
-    from: path.join(paths.src, 'assets/files/*'),
-    to: path.join(paths.build)
+    from: path.join(paths.src, "assets/files/*"),
+    to: path.join(paths.build),
   },
-]
+];
 
 const uglifyConfig = {
   sourceMap: false,
   warnings: false,
   mangle: true,
-  minimize: true
-}
+  minimize: true,
+};
 
 const htmlConfig = {
-  template: path.join(paths.src, 'index.html'),
-  minify : {
-    collapseWhitespace: true
-  }
-}
+  template: path.join(paths.src, "index.html"),
+  minify: {
+    collapseWhitespace: true,
+  },
+};
 
 const cssConfig = {
   cssProcessorOptions: {
     safe: true,
-  }
-}
+  },
+};
 
 const common = {
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
+    contentBase: path.join(__dirname, "build"),
   },
-  entry: path.join(paths.src, 'index.js'),
+  entry: path.join(paths.src, "index.js"),
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.styl']
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".styl"],
   },
   module: {
     rules: [
@@ -54,103 +54,116 @@ const common = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['env']
-          }
-        }
+            presets: ["env"],
+          },
+        },
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "markdown-loader",
+            options: {
+              /* your options here */
+            },
+          },
+        ],
       },
       {
         test: /\.(ts)$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'awesome-typescript-loader',
+          loader: "awesome-typescript-loader",
           options: {
             useCache: false,
-          }
-        }
+          },
+        },
       },
       {
         test: /\.(styl)$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'stylus-loader'
-          ],
-        })
+          fallback: "style-loader",
+          use: ["css-loader", "stylus-loader"],
+        }),
       },
       {
         test: /\.(css)$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
+          fallback: "style-loader",
+          use: "css-loader",
+        }),
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         exclude: /(node_modules)/,
         use: [
           {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      }
-    ]
+            loader: "file-loader",
+            options: {},
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CopyWebpackPlugin(pathsToCopy),
     new CleanWebpackPlugin([paths.build]),
     new HtmlWebpackPlugin(htmlConfig),
     new OptimizeCssAssetsPlugin(cssConfig),
-    new ExtractTextPlugin('styles.[contenthash].css'),
-  ]
+    new ExtractTextPlugin("styles.[contenthash].css"),
+  ],
 };
 
 const devSettings = {
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
     historyApiFallback: true,
   },
   output: {
     path: paths.build,
-    filename: 'bundle.[hash].js',
-    publicPath: '/'
+    filename: "bundle.[hash].js",
+    publicPath: "/",
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new CleanWebpackPlugin([paths.build]),
-  ]
-}
+  ],
+};
 
 const prodSettings = {
-  devtool: 'source-map',
+  devtool: "source-map",
   output: {
     path: paths.build,
-    filename: 'bundle.[hash].js',
-    publicPath: '/build/'
+    filename: "bundle.[hash].js",
+    publicPath: "/build/",
   },
   plugins: [
-    new webpack.DefinePlugin({ 'process.env': {
-      NODE_ENV: JSON.stringify('production')
-    }}),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
     new webpack.optimize.UglifyJsPlugin(uglifyConfig),
     new webpack.optimize.OccurrenceOrderPlugin(),
-  ]
-}
+  ],
+};
 
 /**
-* Exports
-**/
+ * Exports
+ **/
 
 const TARGET = process.env.npm_lifecycle_event;
 process.env.BABEL_ENV = TARGET;
 
-if (TARGET === 'start') {
-  module.exports = merge(common, devSettings)
+if (TARGET === "start") {
+  module.exports = merge(common, devSettings);
 }
 
-if (TARGET === 'build' || TARGET === "develop" || !TARGET) {
-  module.exports = merge(common, prodSettings)
+if (TARGET === "build" || TARGET === "develop" || !TARGET) {
+  module.exports = merge(common, prodSettings);
 }
